caae6902fe0468cc812d76d43dc7b2774a9b78b4
Fred Sauer <fredsa@google.com>
2013-08-06 13:57:43 -0700

    fix run script to deal with cloud SDK
    Reviewed in https://codereview.appspot.com/12548045/

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+set -ue
 
-PYTHONPATH=$(dirname $(which dev_appserver.py)) python scripts/run_tests.py
+DEVAPPSERVER=$(which dev_appserver.py) \
+  || (echo "ERROR: dev_appserver.py must be in your PATH"; exit 1)
+while [ -L $DEVAPPSERVER ]
+do
+  DEVAPPSERVER=$(readlink $DEVAPPSERVER)
+done
+
+BIN_DIR=$(dirname $DEVAPPSERVER)
+
+if [ "$(basename $BIN_DIR)" == "bin" ]
+then
+  SDK_HOME=$(dirname $BIN_DIR)
+  if [ -d $SDK_HOME/platform/google_appengine ]
+  then
+    SDK_HOME=$SDK_HOME/platform/google_appengine
+  fi
+else
+  SDK_HOME=$BIN_DIR
+fi
+PYTHONPATH=$SDK_HOME python scripts/run_tests.py
